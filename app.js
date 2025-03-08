7ef422e89ff66e417fcbf5d4651c5460b58c330c
Bruno <brunobeckhauserfagundes@outlook.com>
2025-03-08 15:25:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+alert('Boas vindas ao jogo do número secreto');
+let numLimite = 1000;
+let numeroSecreto = parseInt(Math.random() * numLimite + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto){
+    chute = prompt(`Escolha um número entre 1 e ${numLimite}`);
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`0 número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+let numTentativas = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${numTentativas}`);
+
+
+/*
+if (tentativas > 1){
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+}else{
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+}
+*/
